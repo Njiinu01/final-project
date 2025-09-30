35e5fe5e679f2a99175a5816acd7f6f1c8fa0e44
Maureen Njiinu <109306976+Njiinu01@users.noreply.github.com>
2025-09-30 16:08:51 +0300

    footer.php

---
+++ inc/inc/inc/footer.php
@@ -0,0 +1,5 @@
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/online_cybercrime/assets/js/main.js"></script>
+</body>
+</html>
